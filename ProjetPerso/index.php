d210550ede28e0e734071caf125ef317c9990e4a
Laureen François <33116689+PtitePixel@users.noreply.github.com>
2017-11-10 10:37:20 +0100

    Add files via upload

---
+++ ProjetPerso/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- SEO -->
+  <meta name="title" content="Projet Personel">
+  <meta name="description" content="Une chouette description">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <!-- CSS Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+  <!-- CSS JQuery UI-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css">
+  <!-- mon CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>Mon titre</title>
+</head>
+<body>
+
+  <!-- CDN : jquery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" ></script>
+  <!-- CDN : script bootstrap -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+  <!-- CDN JQuery UI -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
---
+++ ProjetPerso/js/script.js
@@ -0,0 +1,7 @@
+
+$(function(){
+  alert("ok");
+  $("h1").on("click", function(){
+    $(this).effect("shake");
+  })
+})//JQuery ne pas supprimer
